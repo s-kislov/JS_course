019fb734fac9e0437199a085526f50defcef71d2
Sergey Kislov <serejka1991@gmail.com>
2021-07-30 07:48:32 +0300

    Первый урок готов

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
-alert('внес изменения');
 
+
+let money = 0; 
+let income = 50000; 
+let addExpenses = 25000; 
+let deposit = 10000; 
+let mission = "A God"; 
+let period = "forever";
+
+console.log('mission: ', mission);
+
+alert('Первый урок должен быть принят! :)');

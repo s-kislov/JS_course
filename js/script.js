44fccaea5054281f9e17fe44370421066d79bc9e
Sergey Kislov <serejka1991@gmail.com>
2021-07-30 13:39:18 +0300

    Второй урок с переменными готов

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 
+/* Первая часть домашки */
 
-let money = 0; 
-let income = 50000; 
-let addExpenses = 25000; 
-let deposit = 10000; 
-let mission = "A God"; 
-let period = "forever";
+let money = 20000; 
+let income = "Подбираю на улице"; 
+let addExpenses = "Шнурки, кеды, труханы"; 
+let deposit = false; 
+let mission = 1000000; 
+let period = 12;
+let budgedDay = 60000 / 30;
 
-console.log('mission: ', mission);
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-alert('Первый урок должен быть принят! :)');
+/* Вторая часть домашки */
+
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission  + ' ₽');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(' '));
+
+console.log('budgedDay: ', budgedDay);

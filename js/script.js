27bf36d87ff6fe75e2998a18e8d1ff99ffbe0d7c
Sergey Kislov <serejka1991@gmail.com>
2021-08-01 11:40:23 +0300

    прибрался в структуре

--- js/script.js
+++ js/script.js
@@ -1,33 +1,17 @@
-/* Первая часть */
 let num = 266219;
-
-// разбиваю на числв
 let split = num.toString().split('');
-
-console.log(split);
-
-// определяем числа, с которым будем работать во время перебора массива
 let result = 1;
 
-console.log(result);
 
-// сам массив перебора и умножаем число выше на результат перебора
 for (let i = 0; i < split.length; i++) {
    
     result *= split[i];
 
 }
-// выводим получившееся строку + число
+
+console.log(split);
+console.log(result);
 console.log('Произведение цифр числа ' + num + ' = ' + result);
+console.log((result **3) .toString().slice(0, 2));
 
-/* Вторая часть */
-// возводим в степень
-let elevate = result **3;
-
-console.log(elevate);
-
-/* Третья часть */
-// выводим первые 2 числа степени result
-console.log(elevate.toString().slice(0, 2));
-
-// для новой ветки
+// 

54b66f11ec3c5bf148bddc9d95b67dce09c17d46
Sergey Kislov <serejka1991@gmail.com>
2021-08-06 08:58:36 +0300

    4й урок Done

--- js/script.js
+++ js/script.js
@@ -1,71 +1,52 @@
 'use strict';
 
-let money ; 
+let money = Number(prompt('Ваш месячный доход?', 30000));
 let income = 'Подбираю на улице'; 
-let addExpenses ; 
-let deposit ; 
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'); 
+let deposit = confirm('У тебя есть депозит в банке?'); 
 let mission = 1000000; 
 let period = 12;
-let budgedMonth ;
-let budgedDay ;
-let expenses1 ;
-let expenses2 ;
-let amount1 ;
-let amount2 ;
+let expenses1 = prompt('Введите обязательную статью расходов?');
+let amount1 = +prompt('Во сколько ' + expenses1.toLowerCase() + ' обойдется?');
+let expenses2 = prompt('Введите обязательную статью расходов?');
+let amount2 = +prompt('Во сколько ' + expenses2.toLowerCase() + ' обойдется?');
+let budgedMonth = +money - (amount1 + amount2);
+let budgedDay = Math.floor(+budgedMonth / 30);
 let lvlBudget ;
 
-// проверяем на введенное число, если нет, уточняем вопрос, 
-// если нет, ставим ноль за юзера и выводим Алерт
-money = Number(prompt('Ваш месячный доход?', 30000));
+const showTypeOf = function (data) {
+    console.log(data, typeof data);
+};
 
-if (money) {
-    isNaN(money);
-} else {
-    money = Number(prompt('Так какой ваш месячный доход?', 'Введите сумму числом'));
-    if (money) {
-        isNaN(money);
-    } else {
-        money = 0;
-        alert('Не можете сделать это сами, мы ввели 0 за вас :)');
+const getExpensesMonth = function (amounts) {
+    let result = 0;
+    for (let amount of amounts)
+        result += amount;
+    return result;
+};
+
+const getAccumulatedMonth = function (money, amounts) {
+    return money - getExpensesMonth(amounts);
+};
+
+const getTargetMonth = function (accumulatedMonth, mission) {
+    return Math.ceil(mission / accumulatedMonth);
+};
+
+const getStatusIncome = function (budgedDay) {
+    switch (true) {
+        case budgedDay > 1201 :
+            return (' очень высоким уровнем дохода');
+        case 601 < budgedDay < 1200:
+            return (' средним уровнем дохода');
+        case 0 < budgedDay < 600:
+            return (' низким уровнем дохода');
+        case budgedDay < 0:
+            return (' плохим показателем. Вы — банкрот.');
+        default :
+            return (' странным бюджетом, т.к мы не смогли его определить.');
     }
-}
-
-// выполняем вопрос про расходы
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую')
-
-// запрос про депозит 
-deposit = confirm('У тебя есть депозит в банке?');
-
-// вопросы про бюджет и затраты
-expenses1 = prompt('Введите обязательную статью расходов?');
-amount1 = prompt('Во сколько ' + expenses1.toLowerCase() + ' обойдется?');
-expenses2 = prompt('Введите обязательную статью расходов?');
-amount2 = prompt('Во сколько ' + expenses2.toLowerCase() + ' обойдется?');
-
-// бюджет на месяц за минусом расходов
-budgedMonth = (Number(money) - (Number(amount1) + Number(amount2)));
-
-// бюджет на день
-budgedDay = Math.floor(+budgedMonth / 30);
-
-// проверка условий по бюджету
-switch (true) {
-    case budgedDay > 1201 :
-        lvlBudget = ' очень высоким уровнем дохода';
-        break;
-    case 601 < budgedDay < 1200:
-        lvlBudget = ' средним уровнем дохода';
-        break;
-    case 0 < budgedDay < 600:
-        lvlBudget = ' низким уровнем дохода';
-        break;
-    case budgedDay < 0:
-        lvlBudget = ' плохим показателем. Вы — банкрот.';
-        break;
-    default :
-        lvlBudget = ' странным бюджетом, т.к мы не смогли его определить.';
-}
-
+};
 
 
 // вывод в консол
@@ -75,6 +56,8 @@ console.log(expenses1 + ' нам обходится в ' + amount1 + '₽');
 console.log(expenses2 + ' нам обходится в ' + amount2 + '₽');
 console.log('А на месяц у нас осталось всего ' + budgedMonth + '₽');
 console.log('Через ' + Math.ceil(+mission / +budgedMonth) + ' месяцев я добьюсь цели в ' + mission + '₽');
-console.log('Средний бюджет на день ' + budgedDay + '₽ и это является' + lvlBudget);
+console.log('Средний бюджет на день ' + budgedDay + '₽ и это является' + getStatusIncome(budgedDay));
+
+
 
 
